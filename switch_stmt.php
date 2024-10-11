20daf789653033922262d879354d7ea3a8e7ea9e
smrutika06 <147269208+smrutika06@users.noreply.github.com>
2024-10-11 17:50:48 +0530

    Add files via upload

---
+++ switch_stmt.php
@@ -0,0 +1,21 @@
+<?php
+$friend = "White";
+
+switch($friend){
+    case "White":
+    echo "White";
+    break;
+    case "Yellow":
+    echo "Yellow";
+    break;
+    case "Pink":
+    echo "Pink";
+    break;
+    case "Blue":
+    echo "Blue";
+    break;
+    default:
+    echo "This is not present color";
+}
+    
+?>
