be87c417a9f123a9df6f865cb160a0a3ab534a72
smrutika06 <147269208+smrutika06@users.noreply.github.com>
2024-10-16 17:40:51 +0530

    Add files via upload

---
+++ Ternary_Operator.php
@@ -0,0 +1,7 @@
+<?php
+
+$var = 2;
+//Its use for reduce the line of code using Ternary Operator replace with If Else  
+
+echo ($var == 5) ? "It is Equal Value{$var}" : "It is not  Equal to{$var}";
+?>
